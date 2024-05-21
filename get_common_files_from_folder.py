77da3dd8acd15345a3a0db218cc59a5474489d3b
Martin ACCOU <martin.accou@d3s-consulting.com>
2024-05-21 09:30:10 +0200

    add find_common_files

--- get_common_files_from_folder.py
+++ get_common_files_from_folder.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import logging
 import shutil
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -9,10 +8,24 @@ if TYPE_CHECKING:
     import os
 
 
+def find_common_files(
+    dir1: str | os.PathLike,
+    dir2: str | os.PathLike,
+) -> set[str]:
+    # Convert string paths to Path objects
+    dir1 = Path(dir1)
+    dir2 = Path(dir2)
+    ids_1 = {file.name.split(".")[0]: file for file in dir1.iterdir() if file.is_file()}
+    ids_2 = {file.name.split(".")[0]: file for file in dir2.iterdir() if file.is_file()}
+    common_keys = set(ids_1.keys()) & set(ids_2.keys())
+    print("Found %s common files" % len(common_keys))
+
+    return common_keys
+
+
 def keep_common_files(
     dir1: str | os.PathLike,
     dir2: str | os.PathLike,
-    dir3: str | os.PathLike,
     destination_directory: str | os.PathLike,
 ) -> None:
     # Convert string paths to Path objects
@@ -21,11 +34,13 @@ def keep_common_files(
     destination_directory = Path(destination_directory)
 
     ids_1 = {file.name.split(".")[0]: file for file in dir1.iterdir() if file.is_file()}
+    print("Found %s files in the first directory" % len(ids_1))
     ids_2 = {file.name.split(".")[0]: file for file in dir2.iterdir() if file.is_file()}
+    print("Found %s files in the second directory" % len(ids_2))
 
     # Find the common files
     common_keys = set(ids_1.keys()) & set(ids_2.keys())
-    logging.info("Found %s common files", len(common_keys))
+    print("Found %s common files" % len(common_keys))
 
     destination_directory.mkdir(parents=True, exist_ok=True)
 
@@ -35,10 +50,12 @@ def keep_common_files(
 
 
 def main() -> None:
-    dir1 = "/home/maccou/Bureau/stage-maccou/data/airbus/imgs"
-    dir2 = "/home/maccou/Bureau/stage-maccou/data/airbus/imgs_can"
-    destination_directory = "/home/maccou/Bureau/stage-maccou/data/airbus/stls"
+    dir1 = "/home/maccou/Bureau/stage-maccou/data/mv/old/stl_old_mesher"
+    dir2 = "/home/share/deep-mesh/database/airbus/stl_brep"
+
+    destination_directory = "/home/maccou/Bureau/stage-maccou/data/mv/stl"
     keep_common_files(dir1, dir2, destination_directory)
+    # find_common_files(dir1, dir2)
 
 
 if __name__ == "__main__":
